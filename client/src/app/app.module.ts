5ddea50a1e92b458a189aec152385a68013238c4
Zhdzd <zahidabdullah18@gmail.com>
2022-02-11 09:45:41 +0800

    2nd

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './components/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipe-detail.component';
+import { RecipeAddComponent } from './components/recipe-add.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RecipeService } from './recipe.service';
 
+const appRoutes : Routes = [
+  { path: '', component:RecipeListComponent },
+  { path: 'recipe/:recipeId', component: RecipeDetailComponent},
+  { path: 'add', component: RecipeAddComponent},
+  { path: '**', redirectTo: '/', pathMatch:'full'}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
